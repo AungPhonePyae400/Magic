e2f4f59cdf592a2950666a7af2d83b987079ac25
AungPhonePyae400 <131855421+AungPhonePyae400@users.noreply.github.com>
2026-04-08 23:06:05 +0300

    cmp242Coders
    Fix constructor and method names in cmp242Coders class.

---
+++ cmp242coders.py
@@ -0,0 +1,13 @@
+class cmp242Coders:
+    de__init__(self):
+        self.__grade = None
+
+def set_grade(self, score):
+    self.__grade = score
+
+def get_grade__(self) :
+return self.__grade
+
+student = cmp242Coders ()
+student. set_grade (100)
+print (student.get_grade__())
